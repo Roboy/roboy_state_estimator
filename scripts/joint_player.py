2c64124eac9c88c2697efab8d80b6f0b85f4cedc
missxa <aliona.kharchenko@gmail.com>
2022-03-02 15:06:19 +0100

    record and replay scripts

--- scripts/joint_player.py
+++ scripts/joint_player.py
@@ -2,32 +2,56 @@ import rospy
 import yaml
 from sensor_msgs.msg import JointState
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+from progressbar import progressbar
+
+import argparse
+parser = argparse.ArgumentParser(description='Record a joint position trajectory')
+parser.add_argument('name', default="recorded_joints",
+                    help='enter the name of the file')
+
+args = parser.parse_args()
 
 rospy.init_node("joint_player")
-pub = rospy.Publisher('/joint_targets', JointState, queue_size=1)
+pub = rospy.Publisher('/roboy/oxford/simulation/joint_targets', JointState, queue_size=1)
 
-with open("recorded_joints.yml", 'r') as stream:
+with open(args.name+".yml", 'r') as stream:
     try:
          joints = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
 
+noise = {}
+noisy_targets = {}
 msg = JointState()
 length = 0
 for j in joints:
     msg.name.append(j)
     length = len(joints[j])
+    noise[j] = np.random.normal(0,0.4,length)
 
-for i in range(length):
-    if rospy.is_shutdown():
-        break
+    noisy_targets[j] = [x+y for (x,y) in zip(joints[j],noise[j])]
+    # plt.plot(joints[j])
+    # plt.plot(noisy_targets[j])
+    # plt.show()
+stds = [0.0,0.1,0.2,0.4,0.5]
+for k in range(5):
+    rospy.loginfo("adding noise " + str(stds[k]))
     for j in joints:
-        msg.position.append(joints[j][i])
-        msg.velocity.append(0)
-        msg.effort.append(0)
-    rospy.loginfo(msg.position)
-    pub.publish(msg)
-    msg.position = []
-    msg.effort = []
-    msg.velocity = []
-    time.sleep(0.1)
+        noise[j] = np.random.normal(0,stds[k],length)
+    for i in progressbar(range(length)):
+        if rospy.is_shutdown():
+            break
+        for j in joints:
+            # print(str(joints[j][i]) + " -> " + str(joints[j][i]+noise[j][i]) )
+            # msg.position.append(joints[j][i])
+            msg.position.append(joints[j][i]+noise[j][i])
+            msg.velocity.append(0)
+            msg.effort.append(0)
+        # rospy.loginfo(msg.position)
+        pub.publish(msg)
+        msg.position = []
+        msg.effort = []
+        msg.velocity = []
+        time.sleep(0.1)
--- scripts/joint_recorder.py
+++ scripts/joint_recorder.py
@@ -3,22 +3,33 @@ import yaml
 from sensor_msgs.msg import JointState
 import time
 
-joints = {"shoulder_right_axis0": [], "shoulder_right_axis1": [], "shoulder_right_axis2": [],}
+import argparse
+parser = argparse.ArgumentParser(description='Record a joint position trajectory')
+parser.add_argument('name', default="recorded_joints",
+                    help='enter the name of the file')
+
+args = parser.parse_args()
+
+
+# joints = {"shoulder_right_axis0": [], "shoulder_right_axis1": [], "shoulder_right_axis2": [],}
+joints = {"axis0": [], "axis1": [], "axis2": [],}
 def cb(data):
     for i in range(len(data.name)):
-
         # if joints[data.name[i]] is None:
         #     joints[data.name[i]] = []
-        joints[data.name[i]].append(data.position[i])
+        if data.name[i] in joints.keys():
+            joints[data.name[i]].append(data.position[i])
     time.sleep(0.1)
 
 rospy.init_node("joint_recorder")
-s = rospy.Subscriber('/external_joint_states', JointState, cb, queue_size=1)
+s = rospy.Subscriber('/roboy/oxford/sensing/external_joint_states', JointState, cb, queue_size=1)
 
 while not rospy.is_shutdown():
     rospy.loginfo_throttle(1,"recording external_joint_states")
 # raw_input("Press Enter to start recording...")
 # while()
-
-with open('recorded_joints.yml', 'w') as outfile:
+filename = args.name+'.yml'
+with open(args.name+'.yml', 'w') as outfile:
     yaml.dump(joints, outfile, default_flow_style=False)
+
+rospy.loginfo("saved to "+ filename)
